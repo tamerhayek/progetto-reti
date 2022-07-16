e3872e40807f625d1ec98dd4e96771d75542eddb
samuelecervo <samuelecervomaker@gmail.com>
2022-07-16 18:56:16 +0200

    statistiche categorie

--- containers/postgres/init.sql
+++ containers/postgres/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     nome character varying(30) NOT NULL,
     cognome character varying(30) NOT NULL,
@@ -7,4 +7,10 @@ CREATE TABLE users (
     password character varying(100) NOT NULL,
     logged_with character varying(10) DEFAULT 'local',
     punteggio integer DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS statistichecategorie (
+    id SERIAL PRIMARY KEY,
+    nome character varying(30) NOT NULL,
+    rating integer DEFAULT 0
 );
