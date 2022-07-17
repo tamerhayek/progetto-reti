d3301a249f2188d6627d08327396044e760dbc75
Tamer <tamerhayek15@gmail.com>
2022-07-17 21:37:32 +0200

    try with session env

--- containers/node/server.js
+++ containers/node/server.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
     session({
-        secret: "session",
+        secret: process.env.SESSION_SECRET_KEY,
         secure: false,
         resave: false,
         saveUninitialized: false,
